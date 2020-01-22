3c58e73402ac10017724d471845671261d9a5e22
victory316 <csh153@gmail.com>
2020-01-22 11:58:46 +0900

    creating branch for no aac

--- app/src/main/java/com/example/githubsearcher_2/view/adapter/UserDataAdapter.kt
+++ app/src/main/java/com/example/githubsearcher_2/view/adapter/UserDataAdapter.kt
@@ -1,9 +1,7 @@
 package com.example.githubsearcher_2.view.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubsearcher_2.R
